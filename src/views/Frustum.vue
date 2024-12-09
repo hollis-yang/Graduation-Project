7a00906111fbfdab2e25c547d3b35ff6be89faff
hollisyang <hollis2002@qq.com>
2024-12-09 17:05:00 +0800

    摄像头方向指示线

--- src/views/Frustum.vue
+++ src/views/Frustum.vue
@@ -4,19 +4,21 @@
     <div class="inner">
       <div class="title">参数控制</div>
       <div class="position">
-        <span class="position-input">摄像头位置</span>
+        <span class="position-input">摄像头位置与方向</span>
         <div>
-          <el-input style="width: 250px; margin-right: 20px;"></el-input>
-          <el-button>修改</el-button>
+          <el-input
+            style="width: 250px; margin-right: 20px;"
+            v-model="camPosition"></el-input>
+          <el-button @click="addLine(camPosition)">修改</el-button>
         </div>
       </div>
       <div class="heading">
         <span class="heading-slide">方向角</span>
-        <el-slider v-model="headingAngle" />
+        <el-slider v-model="headingAngle" :max="360" :min="0" />
       </div>
       <div class="pitch">
         <span class="pitch-slide">俯仰角</span>
-        <el-slider v-model="pitchAngle" />
+        <el-slider v-model="pitchAngle" :max="90" :min="-90" />
       </div>
       <div class="horizontal">
         <span class="horizontal-slide">水平视场角</span>
@@ -35,13 +37,74 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import * as Cesium from 'cesium'
+import 'element-plus/dist/index.css'
+import { ElInput, ElButton, ElSlider } from 'element-plus'
+
+let viewer
+
+const camPosition = ref('117.205457,31.842984,63.9')
+const headingAngle = ref(88.5)
+const pitchAngle = ref(-49.5)
+const horizontalAngle = ref(30)
+const verticalAngle = ref(30)
+const projDistance = ref([0, 1000])
+
+// 添加线
+function addLine(camPosition) {
+  // 删除上一次
+  const prevLine = viewer.entities.getById('line')
+  if (prevLine) {
+    viewer.entities.remove(prevLine)
+  }
+
+  // 提取参数
+  const [lon, lat, additionalHeight] = camPosition.split(',').map(Number)
+  console.log(lon, lat, additionalHeight)
+  const heading = Cesium.Math.toRadians(headingAngle.value)
+  const pitch = Cesium.Math.toRadians(pitchAngle.value)
+
+  // 高度采样
+  const terrainProvider = viewer.terrainProvider
+  const startPosition = [Cesium.Cartographic.fromDegrees(lon, lat)]
+
+  Cesium.sampleTerrainMostDetailed(terrainProvider, startPosition).then((updatePosition) => {
+    const terrainHeight = updatePosition[0].height
+    const height = terrainHeight + additionalHeight
+
+    // 方向向量
+    const startCartesian = Cesium.Cartesian3.fromDegrees(lon, lat, height)
+    const direction = new Cesium.Cartesian3()
+    const matrix = Cesium.Transforms.headingPitchRollToFixedFrame(startCartesian, new Cesium.HeadingPitchRoll(heading, pitch, 0))
+    Cesium.Matrix4.getRotation(matrix, new Cesium.Matrix3())
+    Cesium.Cartesian3.fromElements(
+      Cesium.Matrix4.getColumn(matrix, 0, new Cesium.Cartesian3()).x,
+      Cesium.Matrix4.getColumn(matrix, 0, new Cesium.Cartesian3()).y,
+      Cesium.Matrix4.getColumn(matrix, 0, new Cesium.Cartesian3()).z,
+      direction
+    )
+
+    // 计算终点
+    const endCartesian = Cesium.Cartesian3.add(startCartesian, Cesium.Cartesian3.multiplyByScalar(direction, 1000, new Cesium.Cartesian3()), new Cesium.Cartesian3())
+
+    // 添加线实体
+    viewer.entities.add({
+      id: 'line',
+      polyline: {
+        positions: [startCartesian, endCartesian],
+        width: 5,
+        material: Cesium.Color.RED,
+      }
+    })
+    viewer.zoomTo(viewer.entities)
+  })
+}
 
 onMounted(() => {
   Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0MGMyMWM0YS1lNDEyLTRjNWYtYTY3OS1jOTZkOWM1OThjYTEiLCJpZCI6MTkxMTMxLCJpYXQiOjE3MDU5MDY1ODN9.Tq3eObtuZJiqt4rDl-srQkMfz-WP9_EvKEPa_UvOI5s'
 
-  const viewer = new Cesium.Viewer('cesiumContainer', {
+  viewer = new Cesium.Viewer('cesiumContainer', {
     animation: false,
     baseLayerPicker: false,
     fullscreenButton: false,
