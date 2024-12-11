f845a34347e3743db148ce944ea7b6df095008c7
hollisyang <hollis2002@qq.com>
2024-12-12 00:03:25 +0800

    接上真实数据（浏览器屏蔽问题待解决）

--- src/views/VideoShadowMap copy.vue
+++ src/views/VideoShadowMap copy.vue
@@ -5,7 +5,6 @@
 <script setup>
 import { onMounted } from 'vue'
 import * as Cesium from 'cesium'
-import Hls from 'hls.js'
 
 onMounted(() => {
   Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0MGMyMWM0YS1lNDEyLTRjNWYtYTY3OS1jOTZkOWM1OThjYTEiLCJpZCI6MTkxMTMxLCJpYXQiOjE3MDU5MDY1ODN9.Tq3eObtuZJiqt4rDl-srQkMfz-WP9_EvKEPa_UvOI5s'
@@ -30,7 +29,7 @@ onMounted(() => {
   viewer.clock.shouldAnimate = true
   viewer.scene.globe.depthTestAgainstTerrain = true
 
-  let p = [118.166,30.143,1800];
+  let p = [117.205457, 31.842984, 53.9];
   p = Cesium.Cartesian3.fromDegrees(p[0], p[1], p[2]);
   let camera = new Cesium.Camera(viewer.scene);
 
@@ -94,32 +93,8 @@ onMounted(() => {
   videoEle.setAttribute("loop", true);
   videoEle.setAttribute("autoplay", true);
   document.body.appendChild(videoEle);
-  // videoEle.setAttribute("src", "/lukou.mp4");
+  videoEle.setAttribute("src", "/lukou.mp4");
   videoEle.style.cssText = "position:absolute;left:0px;top:0px;width:320px;height:240px;display:none";
-
-  if (Hls.isSupported()) {
-    const hls = new Hls()
-    hls.loadSource('http://hls01open.ys7.com/openlive/50c94278dd444de194c8f9251b4db27e.m3u8')
-    hls.attachMedia(videoEle)
-    hls.on(Hls.Events.MANIFEST_PARSED, function () {
-      videoEle.play().then(() => {
-        console.log('Video playing')
-      }).catch(error => {
-        console.error('Video play error:', error)
-      })
-    })
-  } else if (videoEle.canPlayType('application/vnd.apple.mpegurl')) {
-    videoEle.src = 'http://hls01open.ys7.com/openlive/50c94278dd444de194c8f9251b4db27e.m3u8'
-    videoEle.addEventListener('loadedmetadata', function () {
-      videoEle.play().then(() => {
-        console.log('Video playing')
-      }).catch(error => {
-        console.error('Video play error:', error)
-      })
-    })
-  } else {
-    console.error('HLS.js is not supported in this browser.')
-  }
 
   // 等待视频加载完数据
   videoEle.addEventListener('loadeddata', () => {
@@ -127,24 +102,8 @@ onMounted(() => {
     uniform sampler2D colorTexture;
     varying vec2 v_textureCoordinates;
     uniform sampler2D depthTexture;
-    uniform sampler2D shadowMap_texture; 
     uniform sampler2D videoTexture; 
     uniform mat4 shadowMap_matrix;
-    uniform vec4 shadowMap_lightPositionEC;
-    uniform vec4 shadowMap_normalOffsetScaleDistanceMaxDistanceAndDarkness;
-    uniform vec4 shadowMap_texelSizeDepthBiasAndNormalShadingSmooth;
-
-    float _czm_shadowVisibility(sampler2D shadowMap, czm_shadowParameters shadowParameters) {
-      float depthBias = shadowParameters.depthBias;
-      float depth = shadowParameters.depth;
-      float nDotL = shadowParameters.nDotL;
-      float normalShadingSmooth = shadowParameters.normalShadingSmooth;
-      float darkness = shadowParameters.darkness;
-      vec2 uv = shadowParameters.texCoords;
-      depth -= depthBias; 
-      float visibility = czm_shadowDepthCompare(shadowMap, uv, depth); 
-      return visibility;
-    }
 
     void main() { 
       vec4 color = texture2D(colorTexture, v_textureCoordinates);
@@ -158,35 +117,18 @@ onMounted(() => {
       vec4 eyeCoordinate4 = czm_windowToEyeCoordinates(gl_FragCoord.xy, depth);  
       vec4 positionEC = eyeCoordinate4 / eyeCoordinate4.w;
 
-      // 开始 
-      vec3 normalEC = vec3(1.);
-      czm_shadowParameters shadowParameters; 
-      shadowParameters.texelStepSize = shadowMap_texelSizeDepthBiasAndNormalShadingSmooth.xy; 
-      shadowParameters.depthBias = shadowMap_texelSizeDepthBiasAndNormalShadingSmooth.z; 
-      shadowParameters.normalShadingSmooth = shadowMap_texelSizeDepthBiasAndNormalShadingSmooth.w; 
-      shadowParameters.darkness = shadowMap_normalOffsetScaleDistanceMaxDistanceAndDarkness.w;
-      shadowParameters.depthBias *= max(depth * .01, 1.);
-      vec3 directionEC = normalize(positionEC.xyz - shadowMap_lightPositionEC.xyz); 
-      float nDotL = clamp(dot(normalEC, -directionEC), 0.0, 1.0); 
-
+      // 阴影位置变换，作为应用视频纹理的区域
       vec4 shadowPosition = shadowMap_matrix * positionEC; 
       shadowPosition /= shadowPosition.w; 
 
-      if (any(lessThan(shadowPosition.xyz, vec3(0.0))) || any(greaterThan(shadowPosition.xyz, vec3(1.0)))) { 
-        return; 
-      } 
-
-      shadowParameters.texCoords = shadowPosition.xy; 
-      shadowParameters.depth = shadowPosition.z; 
-      shadowParameters.nDotL = nDotL; 
-      float visibility = _czm_shadowVisibility(shadowMap_texture, shadowParameters);  
-
-      if (visibility == 1.0) {
-        vec2 rotatedCoords = vec2(1.0 - shadowPosition.x, 1.0 - shadowPosition.y);
-        vec4 videoColor = texture2D(videoTexture, rotatedCoords);
+      // 检查是否在阴影区域内（原先的区域）
+      if (all(greaterThanEqual(shadowPosition.xyz, vec3(0.0))) && all(lessThanEqual(shadowPosition.xyz, vec3(1.0)))) {
+        vec2 videoCoords = vec2(1.0 - shadowPosition.x, 1.0 - shadowPosition.y);
+        vec4 videoColor = texture2D(videoTexture, videoCoords);
         gl_FragColor = vec4(videoColor.xyz, 1.);
       } 
-    }`;
+    }
+    `;
 
     // 创建视频纹理
     let videoTexture = new Cesium.Texture({
@@ -209,29 +151,7 @@ onMounted(() => {
 
     let uniforms = {
       videoTexture: videoTexture,
-      shadowMap_texture: () => shadowMap._shadowMapTexture,
       shadowMap_matrix: () => shadowMap._shadowMapMatrix,
-      shadowMap_lightPositionEC: () => shadowMap._lightPositionEC,
-      shadowMap_texelSizeDepthBiasAndNormalShadingSmooth: () => {
-        const texelStepSize = new Cesium.Cartesian2(
-          1.0 / shadowMap._textureSize.x,
-          1.0 / shadowMap._textureSize.y
-        );
-        return new Cesium.Cartesian4(
-          texelStepSize.x,
-          texelStepSize.y,
-          shadowMap._primitiveBias.depthBias,
-          shadowMap._primitiveBias.normalShadingSmooth
-        );
-      },
-      shadowMap_normalOffsetScaleDistanceMaxDistanceAndDarkness: () => {
-        return new Cesium.Cartesian4(
-          shadowMap._primitiveBias.normalOffsetScale,
-          shadowMap._distance,
-          shadowMap.maximumDistance,
-          shadowMap._darkness
-        );
-      }
     };
 
     let postProcessStage = new Cesium.PostProcessStage({
