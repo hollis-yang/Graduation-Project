75725f3a3bfb0e10feab12bcac2af77fbc23cd05
hollisyang <hollis2002@qq.com>
2025-01-26 10:41:51 +0800

    建立视频列表并遍历添加

--- src/views/VideoMulti.vue
+++ src/views/VideoMulti.vue
@@ -3,12 +3,43 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import * as Cesium from 'cesium'
 import Hls from 'hls.js'
 import { createVideo3D } from '@/utils/addMultiVideo'
 
 let viewer = null
+
+const cameraList = ref([
+  {
+    id: 'camera1',
+    position: [121.11124-0.001, 31.14121, 100.49794],
+    stanceOption: {
+      heading: 106.9,
+      pitch: -72.3,
+      roll: 0,
+      fov: 28.9,
+      aspectRatio: 2.1,
+      distance: 1000,
+      frustumShow: true
+    },
+    videoUrl: 'http://hls01open.ys7.com/openlive/78e31911faee4c9382a669542f7fe9f0.m3u8'
+  },
+  {
+    id: 'camera2',
+    position: [121.11124+0.001, 31.14121, 100.49794],
+    stanceOption: {
+      heading: 106.9,
+      pitch: -72.3,
+      roll: 0,
+      fov: 40,
+      aspectRatio: 3,
+      distance: 1000,
+      frustumShow: true
+    },
+    videoUrl: 'http://hls01open.ys7.com/openlive/78e31911faee4c9382a669542f7fe9f0.m3u8'
+  }
+])
 
 // 显示信息的函数
 function showPopupInfo(position) {
@@ -41,8 +72,7 @@ function hidePopupInfo() {
 
 onMounted(() => {
   Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0MGMyMWM0YS1lNDEyLTRjNWYtYTY3OS1jOTZkOWM1OThjYTEiLCJpZCI6MTkxMTMxLCJpYXQiOjE3MDU5MDY1ODN9.Tq3eObtuZJiqt4rDl-srQkMfz-WP9_EvKEPa_UvOI5s'
-
-  // 初始化 Cesium Viewer
+  
   viewer = new Cesium.Viewer('cesiumContainer', {
     animation: false,
     baseLayerPicker: false,
@@ -73,52 +103,48 @@ onMounted(() => {
   })
   viewer.imageryLayers.addImageryProvider(tdtAnnoLayer)
 
-  // 创建视频元素
-  let videoEle = document.createElement('video')
-  videoEle.setAttribute('muted', true)
-  videoEle.setAttribute('loop', true)
-  videoEle.setAttribute('autoplay', true)
-  document.body.appendChild(videoEle)
-  // videoEle.setAttribute('src', '/lukou.mp4')
-  videoEle.style.cssText = 'position:absolute;left:0px;top:0px;width:320px;height:240px;display:none'
 
-  if (Hls.isSupported()) {
-    const hls = new Hls()
-    hls.loadSource('http://hls01open.ys7.com/openlive/78e31911faee4c9382a669542f7fe9f0.m3u8')
-    hls.attachMedia(videoEle)
-    hls.on(Hls.Events.MANIFEST_PARSED, function () {
-      videoEle.play().then(() => {
-        console.log('Video playing')
-      })
-    })
-  } else if (videoEle.canPlayType('application/vnd.apple.mpegurl')) {
-    videoEle.src = 'http://hls01open.ys7.com/openlive/78e31911faee4c9382a669542f7fe9f0.m3u8'
-    videoEle.addEventListener('loadedmetadata', function () {
-      videoEle.play().then(() => {
-        console.log('Video playing')
-      })
-    })
-  }
+  /**
+   * 遍历cameraList添加视频
+   */
+  cameraList.value.forEach(camera => {
+    // 创建视频元素
+    let videoEle = document.createElement('video')
+    videoEle.setAttribute('muted', true)
+    videoEle.setAttribute('loop', true)
+    videoEle.setAttribute('autoplay', true)
+    document.body.appendChild(videoEle)
+    videoEle.style.cssText = 'position:absolute;left:0px;top:0px;width:320px;height:240px;display:none'
 
-  // 视频加载完成后创建 3D 视频对象
-  videoEle.addEventListener('loadeddata', () => {
-    let p = [121.11124, 31.14121, 100.49794]
-    let stanceOption = {
-      heading: 106.9,
-      pitch: -72.3,
-      roll: 0,
-      fov: 28.9,
-      aspectRatio: 2.1,
-      distance: 1000,
-      frustumShow: true
-    }  // 探头姿态
-    let p1 = Cesium.Cartesian3.fromDegrees(p[0] - 0.001, p[1], p[2])
-    createVideo3D(p1, viewer, videoEle, 'camera1', stanceOption)
-    let p2 = Cesium.Cartesian3.fromDegrees(p[0] + 0.001, p[1], p[2])
-    createVideo3D(p2, viewer, videoEle, 'camera2', stanceOption)
+    // 使用hls.js播放视频
+    if (Hls.isSupported()) {
+      const hls = new Hls()
+      hls.loadSource(camera.videoUrl)
+      hls.attachMedia(videoEle)
+      hls.on(Hls.Events.MANIFEST_PARSED, function () {
+        videoEle.play().then(() => {
+          console.log('Video playing')
+        })
+      })
+    } else if (videoEle.canPlayType('application/vnd.apple.mpegurl')) {
+      videoEle.src = camera.videoUrl
+      videoEle.addEventListener('loadedmetadata', function () {
+        videoEle.play().then(() => {
+          console.log('Video playing')
+        })
+      })
+    }
+
+    // 视频加载完成后创建 3D 视频对象
+    videoEle.addEventListener('loadeddata', () => {
+      let p = camera.position
+      let p1 = Cesium.Cartesian3.fromDegrees(p[0], p[1], p[2])
+      createVideo3D(p1, viewer, videoEle, camera.id, camera.stanceOption)
+    })
   })
 
-  // 飞到指定位置
+
+  // 飞到默认位置
   viewer.camera.flyTo({
     destination: Cesium.Cartesian3.fromDegrees(121.11124, 31.14121, 100.49794),
     orientation: {
@@ -129,6 +155,10 @@ onMounted(() => {
     duration: 1,
   })
 
+
+  /**
+   * 监听事件
+   */
   viewer.scene.canvas.addEventListener('click', (event) => {
     // 获取鼠标点击位置
     const screenPosition = new Cesium.Cartesian2(event.clientX, event.clientY)
